4ee551ccd7112a201d0455c04b825829beefba09
Eric <eric@baochi.xin>
2023-04-28 10:48:09 +0800

    add dev.yml

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { ConfigService } from '@nestjs/config';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
-  const port = configService.get('PORT');
+  const port = configService.get('PORT') || 8080;
   // Disable CORS
   app.enableCors({
     origin: true,
